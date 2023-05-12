c37795b5c59e41422cbec60db8be8d8fae740394
Destycoco <destibright1@gmail.com>
2023-05-12 23:31:19 +0100

    create the function that will be responsible for requesting option from the computer

---
+++ main.js
@@ -0,0 +1,15 @@
+
+function computerChoice(){
+	let randomNumber = Math.ceil(Math.random() * 3);
+	if (randomNumber === 1){
+		return "rock";
+	}
+	else if(randomNumber === 2){
+		return "paper";
+	}
+	else{
+		return "scissors";
+	}
+}
+computerChoice();
+
